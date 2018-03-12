471516514e65f93c197616f516ce0a6049b370a1
CM Student <cmstudent@UHWO-ACM-C-03.local>
2018-03-12 11:02:09 -1000

    FINISHED part 3

--- Hackwich6/ViewController.swift
+++ Hackwich6/ViewController.swift
@@ -13,12 +13,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     //create a variable that can hold the value of the slider
     var currentValue: Int = 0
+    //create a variable that's gonna hold our target value
+    var targetValue: Int = 0
     
+    @IBOutlet weak var targetLabel: UILabel!
+    func updateTargetLabel
+    updateTargetlabel 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //setting current value to the slider's value
+        currentValue = lroundf(slider.value)
+        
+        //call start new round
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,9 +42,15 @@ class ViewController: UIViewController {
         currentValue = lroundf(slider.value)
     }
     
+    func startNewRound () {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
     @IBAction func myGuessButtonPressed(_ sender: Any) {
         //new variable message that displays a message that includes currentValue
-        let message = "The value is: \(currentValue)"
+        let message = "The value is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
         //create alert view
         let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
@@ -43,8 +59,11 @@ class ViewController: UIViewController {
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         //add the button to the alert view
         alert.addAction(action)
+        
         //present alertview on the screen
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
 }
 
